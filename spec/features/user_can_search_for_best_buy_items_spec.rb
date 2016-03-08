2065e43958185a135b9ce7147729708ec9e136dc
Beth Secor <elizabeth.a.secor@gmail.com>
2016-03-08 10:11:06 -0700

    Search form for best buy products added to view

--- spec/features/user_can_search_for_best_buy_items_spec.rb
+++ spec/features/user_can_search_for_best_buy_items_spec.rb
@@ -4,13 +4,13 @@ RSpec.feature "UserCanSearchForBestBuyItems", type: :feature do
   it "should be able to search for items in best buy" do
     visit root_path
 
-    fill_in "Best Buy Products", with: "sennheiser"
+    fill_in "Product", with: "sennheiser"
+    VCR.use_cassette("best_buy#sennheiser_products") do
     click_on "Search"
 
     expect(current_path).to eq search_path
 
     expect(page).to have_content("15 Items")
-
-    expect(page).to have_content
+    end
   end
 end
